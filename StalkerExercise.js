00a8d47a4f0993b493c63479f22e301243ada9e8
Jonathan Lee <jonathanleeee@gmail.com>
2017-11-21 21:05:02 -0800

    now this app asking for your first and last name, age

--- StalkerExercise.js
+++ StalkerExercise.js
@@ -1,1 +1,8 @@
-alert("connected!");
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name bro?");
+var age = prompt("What's your age bro?")
+
+alert("Hi " + firstName + " " + lastName + ", you are " + age + " years old.");
+
+console.log("Your full name is " + firstName + ' ' + lastName);
+console.log("You are " + age + " old");
